0f725f916460930be1c5da01c3b90c73b6da49f0
Tamas Eppel <tamas.ferenz.eppel@zalando.de>
2016-01-12 16:02:06 +0100

    updating taupage discovery

--- src/main/kotlin/org/zalando/axiom/krueger/service/discovery/TaupageDiscovery.kt
+++ src/main/kotlin/org/zalando/axiom/krueger/service/discovery/TaupageDiscovery.kt
@@ -1,5 +1,6 @@
 package org.zalando.axiom.krueger.service.discovery
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.netflix.hystrix.HystrixCommand
 import com.netflix.hystrix.HystrixCommandKey
@@ -12,6 +13,32 @@ import org.zalando.axiom.krueger.ApplicationGroup
 import org.zalando.axiom.krueger.logging.logger
 import java.util.*
 
+private fun parseApplicationNode(applicationNode: JsonNode): List<Application> {
+    val result = LinkedList<Application>()
+
+    val ip = applicationNode.get("key").asText().replace("/taupage/", "")
+    val applicationData = Json.mapper.readTree(applicationNode.get("value").asText())
+    val applicationId = applicationData.get("application_id").asText()
+
+    applicationData.get("ports").fields()?.let { fields ->
+        fields.forEach { field ->
+            result.add(Application(applicationId, "$ip:${field.key}"))
+        }
+    }
+
+    return result
+}
+
+fun parseJsonString(jsonString: String): Set<ApplicationGroup> {
+    val result = HashSet<ApplicationGroup>()
+    val taupageNode = Json.mapper.readTree(jsonString)
+    val applicationsNode = taupageNode.get("node").get("nodes")
+
+    val applications = applicationsNode.flatMap { parseApplicationNode(it) }
+    val applicationGroups = applications.groupBy { it.applicationId }
+    applicationGroups.forEach { entry -> result.add(ApplicationGroup(entry.key, entry.value.toSet())) }
+    return result
+}
 
 class TaupageDiscoveryCommand(val etcdDomain: String, val mapper: ObjectMapper = ObjectMapper())
 : HystrixCommand<Set<ApplicationGroup>>(HystrixCommand.Setter.withGroupKey({ "krueger" }).andCommandKey(HystrixCommandKey({ "taupageDiscovery" }))) {
@@ -32,17 +59,7 @@ class TaupageDiscoveryCommand(val etcdDomain: String, val mapper: ObjectMapper =
         }
 
         val jsonString = EntityUtils.toString(response.entity)
-        val taupageNode = Json.mapper.readTree(jsonString)
-        val applicationsNode = taupageNode.get("node").get("nodes")
-
-        val applications = applicationsNode.map { applicationNode ->
-            val ip = applicationNode.get("key").asText().replace("/taupage/", "")
-            val applicationData = Json.mapper.readTree(applicationNode.get("value").asText())
-            val applicationId = applicationData.get("application_id").asText()
-            Application(applicationId, ip)
-        }
-        result.add(ApplicationGroup(applications.first().applicationId, applications.toSet()))
-        return result
+        return parseJsonString(jsonString)
     }
 
 }
